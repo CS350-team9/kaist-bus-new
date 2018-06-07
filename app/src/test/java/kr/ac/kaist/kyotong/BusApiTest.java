9b0786edbf760e26fec6cc6b7f61b32296896d37
pastelmind <keepyourhonor@gmail.com>
2018-06-07 22:46:41 +0900

    Testing 기능 도입

---
+++ app/src/test/java/kr/ac/kaist/kyotong/BusApiTest.java
@@ -0,0 +1,25 @@
+package kr.ac.kaist.kyotong;
+
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BusApiTest {
+    public static String TAG = BusApiTest.class.getName();
+    @Test
+    public void busApi_Sample_TestTrue() {
+        assertTrue(true);
+        assertFalse(1 == 0);
+        assertFalse(1 == 0);
+        assertFalse(1 == 0);
+        assertFalse(1 == 0);
+        assertFalse(1 == 1);
+        assertFalse(1 == 0);
+        assertFalse(1 == 0);
+        assertFalse(1 == 0);
+        assertFalse(1 == 1);
+        assertFalse(1 == 0);
+        assertFalse(1 == 0);
+        assertFalse(1 == 0);
+    }
+}
