c7d5a58424fc99640df716da09adbccb4c69787d
pastelmind <keepyourhonor@gmail.com>
2018-06-07 23:19:45 +0900

    테스트용 android.util.Log의 mock 생성, BusApi 테스트 작업 중

--- app/src/test/java/kr/ac/kaist/kyotong/BusApiTest.java
+++ app/src/test/java/kr/ac/kaist/kyotong/BusApiTest.java
@@ -1,25 +1,23 @@
 package kr.ac.kaist.kyotong;
 
+import kr.ac.kaist.kyotong.api.BusApi;
+import kr.ac.kaist.kyotong.api.BusRouteData;
+
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class BusApiTest {
-    public static String TAG = BusApiTest.class.getName();
     @Test
-    public void busApi_Sample_TestTrue() {
+    public void busApi_Sample_PrintDebug() {
         assertTrue(true);
         assertFalse(1 == 0);
-        assertFalse(1 == 0);
-        assertFalse(1 == 0);
-        assertFalse(1 == 0);
-        assertFalse(1 == 1);
-        assertFalse(1 == 0);
-        assertFalse(1 == 0);
-        assertFalse(1 == 0);
-        assertFalse(1 == 1);
-        assertFalse(1 == 0);
-        assertFalse(1 == 0);
-        assertFalse(1 == 0);
+        BusApi busApi = new BusApi(R.string.tab_kaist_olev);
+        BusRouteData data = busApi.getResult();
+
+        assertEquals(62, data.buses.size());
+        assertEquals(10, data.stations.size());
     }
 }
