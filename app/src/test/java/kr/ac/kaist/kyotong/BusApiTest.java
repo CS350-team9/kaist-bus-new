15f0e3a8cf8543aed872fc8ce02268b178265b1e
pastelmind <keepyourhonor@gmail.com>
2018-06-08 01:55:32 +0900

    버스가 다니는 정거장의 수와 실제 정거장의 수를 비교하는 테스트 추가

--- app/src/test/java/kr/ac/kaist/kyotong/BusApiTest.java
+++ app/src/test/java/kr/ac/kaist/kyotong/BusApiTest.java
@@ -6,18 +6,53 @@ import kr.ac.kaist.kyotong.api.BusRouteData;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class BusApiTest {
     @Test
-    public void busApi_Sample_PrintDebug() {
-        assertTrue(true);
-        assertFalse(1 == 0);
+    public void busApi_CheckStationCount_OLEV() {
         BusApi busApi = new BusApi(R.string.tab_kaist_olev);
         BusRouteData data = busApi.getResult();
 
-        assertEquals(62, data.buses.size());
-        assertEquals(10, data.stations.size());
+        //BusModel.stations의 길이를 외부에서 구할 방법이 없으므로 비정상적인 방법으로 구해야 한다.
+        int stationCount = 0;
+        try {
+            while (data.buses.get(0).getStation(stationCount) != null)
+                ++stationCount;
+        }
+        catch (IndexOutOfBoundsException e) {}
+
+        assertEquals("총 정거장 수와 버스가 방문하는 정거장 수가 같아야 한다.", data.stations.size(), stationCount);
+    }
+
+    @Test
+    public void busApi_CheckStationCount_Munji() {
+        BusApi busApi = new BusApi(R.string.tab_kaist_sunhwan);
+        BusRouteData data = busApi.getResult();
+
+        //BusModel.stations의 길이를 외부에서 구할 방법이 없으므로 비정상적인 방법으로 구해야 한다.
+        int stationCount = 0;
+        try {
+            while (data.buses.get(0).getStation(stationCount) != null)
+                ++stationCount;
+        }
+        catch (IndexOutOfBoundsException e) {}
+
+        assertEquals("총 정거장 수와 버스가 방문하는 정거장 수가 같아야 한다.", data.stations.size(), stationCount);
+    }
+
+    @Test
+    public void busApi_CheckStationCount_Wolpyeong() {
+        BusApi busApi = new BusApi(R.string.tab_kaist_wolpyeong);
+        BusRouteData data = busApi.getResult();
+
+        //BusModel.stations의 길이를 외부에서 구할 방법이 없으므로 비정상적인 방법으로 구해야 한다.
+        int stationCount = 0;
+        try {
+            while (data.buses.get(0).getStation(stationCount) != null)
+                ++stationCount;
+        }
+        catch (IndexOutOfBoundsException e) {}
+
+        assertEquals("총 정거장 수와 버스가 방문하는 정거장 수가 같아야 한다.", data.stations.size(), stationCount);
     }
 }
